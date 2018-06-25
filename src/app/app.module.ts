022219259e3dde0acd39f4c060518fdbaaabf5ea
Tsimafei Burdzeika <tymofii.burdeiko@gmail.com>
2018-06-25 17:23:11 +0300

    Add CountryService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HotelMappingsComponent } from './hotel-mappings/hotel-mappings.component';
 import { ToursComponent } from './tours/tours.component';
 
-import {Country} from './models/country'
+import {Country} from './models/country';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,11 @@ import {Country} from './models/country'
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [{provide: 'api-host', useValue: 'http://localhost:59911/api/'}],
   bootstrap: [AppComponent]
 })
 
--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from '../models/country';
+import {CountryService} from '../services/country.service';
 
 @Component({
   selector: 'app-tours',
   templateUrl: './tours.component.html',
   styleUrls: ['./tours.component.css']
 })
-export class ToursComponent implements OnInit {
 
-  constructor() {
-    this.countries = [];
+export class ToursComponent implements OnInit {
+  constructor(private countryService: CountryService) {
+    countryService.getCountries().subscribe((data: Country[]) => this.countries = data);
   }
 
   ngOnInit() {
